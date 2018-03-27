cc48a75d63c914897ef9b88ba6d70439ecc82311
katierose9 <kwhitney98@gmail.com>
2018-03-27 19:10:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,36 +9,30 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(myItemName) {
-  var myItemPrice = Math.floor(Math.random() * 100);
-  var item = {itemName: myItemName, itemPrice: myItemPrice};
-  getCart().push(item);
-
-  return `${myItemName} has been added to your cart.`;
-
- }
+function addToCart(item) {
+ var item = generateCartItem(item)
+ getCart().push(item)
+ return `${item.itemName} has been added to your cart.`
+}
 
 function viewCart() {
-  var cart_text = "In your cart, you have";
-  for (var x=0; x<getCart().length; x++) {
-    if ((getCart().length > 1) && (x == getCart().length - 1)) {
-      cart_text = cart_text + " and";
-    }
-    cart_text = cart_text + ` ${getCart()[x].itemName} at $${getCart()[x].itemPrice}`
-    if (x == getCart().length - 1) {
-      cart_text = cart_text + ".";
-    } else {
-      cart_text = cart_text + ",";
-    }
+  var cartDescription = 'In your cart, you have '
+  
+  for (var x=1; x<getCart.length -1; x++)
+
+  if ( getCart().length >= 1 ) {
+    return `cartDescription + ${getCart()[0].itemName} at $${getCart()[0].itemPrice}`
   }
-  if (getCart().length == 0) {
-    return "Your shopping cart is empty.";
-  } else {
-    return cart_text;
+  if ( getCart().length >= 2 ) {
+    return `cartDescription + ${getCart()[x].itemName} at $${getCart()[x].itemPrice}`
   }
+  
+  if getCart().length == 0
+  return "Your shopping cart is empty." // write your code here
 }
 
 function total() {
+  
   // write your code here
 }
 
